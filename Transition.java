6a8e23ba1c789c8a632b0290ad67b990447d2077
= <=>
2024-05-31 11:32:23 -0300

    adicionando a pasta lib

--- Transition.java
+++ Transition.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package automatosTest;
 
-/**
- *
- * @author Ozeias
- */
 public class Transition {
     private int from;
     private String read;
